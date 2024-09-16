7ead7dcd64ba11f670771079e24a5e80d536f9c8
talwindersinghprof <talwindersinghprof@gmail.com>
2024-09-16 18:06:27 +0530

    Create inorder bst succesor.cpp

---
+++ inorder bst succesor.cpp
@@ -0,0 +1,21 @@
+
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) 
+    {
+        TreeNode* successor=nullptr;
+        while(root)
+        {
+            if(p->val>=root->val)
+            {
+                root=root->right;
+            }
+            else 
+            {
+                successor=root;
+                root=root->left;
+            }
+        }
+        return successor;
+    }
+};
